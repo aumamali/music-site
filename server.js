13e5481301b5e1e746f301f5e22d2f360470a4db
Khode_313 <mamalijafari41@gmail.com>
2026-04-15 12:35:18 +0430

    Enhance music app with views and improved like system

--- server.js
+++ server.js
@@ -7,35 +7,76 @@ app.use(express.json());
 
 let musics = [];
 let comments = {};
+let views = {};
 
 // گرفتن موزیک‌ها
 app.get("/musics", (req, res) => {
   res.json(musics);
 });
 
-// اضافه کردن موزیک
+// اضافه کردن موزیک (ادمین)
 app.post("/add-music", (req, res) => {
-  musics.push({ ...req.body, likes: 0 });
+  musics.push({
+    ...req.body,
+    likes: 0,
+    id: Date.now()
+  });
   res.send("ok");
 });
 
-// لایک
+// لایک (هر نفر فقط یکبار)
+let likedUsers = {};
+
 app.post("/like/:id", (req, res) => {
-  musics[req.params.id].likes++;
+  const user = req.body.user;
+  const id = req.params.id;
+
+  if (!likedUsers[id]) likedUsers[id] = [];
+
+  if (likedUsers[id].includes(user)) {
+    return res.send("already liked");
+  }
+
+  likedUsers[id].push(user);
+
+  musics = musics.map(m => {
+    if (m.id == id) m.likes++;
+    return m;
+  });
+
   res.send("liked");
+});
+
+// ویو
+app.post("/view/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (!views[id]) views[id] = 0;
+  views[id]++;
+
+  res.send("viewed");
+});
+
+app.get("/views/:id", (req, res) => {
+  res.json({ views: views[req.params.id] || 0 });
 });
 
 // کامنت
 app.post("/comment/:id", (req, res) => {
-  let id = req.params.id;
+  const id = req.params.id;
+  const { name, text } = req.body;
+
   if (!comments[id]) comments[id] = [];
-  comments[id].push(req.body);
-  res.send("comment added");
+
+  comments[id].push({ name, text });
+
+  res.send("ok");
 });
 
-// گرفتن کامنت
 app.get("/comments/:id", (req, res) => {
   res.json(comments[req.params.id] || []);
 });
 
-app.listen(3000, () => console.log("Server running on 3000"));
+app.listen(3000, () => {
+  console.log("Server running...");
+});
