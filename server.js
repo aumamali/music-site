33a88f7fe673f52284832b53d142f2a7a58a40bd
Khode_313 <mamalijafari41@gmail.com>
2026-04-15 12:31:49 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const path = require("path");
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 10000;
+let musics = [];
+let comments = {};
 
-// static folder
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+// گرفتن موزیک‌ها
+app.get("/musics", (req, res) => {
+  res.json(musics);
 });
+
+// اضافه کردن موزیک
+app.post("/add-music", (req, res) => {
+  musics.push({ ...req.body, likes: 0 });
+  res.send("ok");
+});
+
+// لایک
+app.post("/like/:id", (req, res) => {
+  musics[req.params.id].likes++;
+  res.send("liked");
+});
+
+// کامنت
+app.post("/comment/:id", (req, res) => {
+  let id = req.params.id;
+  if (!comments[id]) comments[id] = [];
+  comments[id].push(req.body);
+  res.send("comment added");
+});
+
+// گرفتن کامنت
+app.get("/comments/:id", (req, res) => {
+  res.json(comments[req.params.id] || []);
+});
+
+app.listen(3000, () => console.log("Server running on 3000"));
