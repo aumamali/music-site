062b7e1ab0a282b5ce47a5c0ee8e2041141efff7
Khode_313 <mamalijafari41@gmail.com>
2026-04-16 12:06:43 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Server is running successfully!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
-app.get("/admin", (req, res) => {
-  res.send("Admin panel is working!");
-});
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
