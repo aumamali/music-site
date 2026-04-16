6b4d357a384876b4d23a6d1af584f16218751607
Khode_313 <mamalijafari41@gmail.com>
2026-04-16 12:13:28 +0430

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// فایل‌های استاتیک
 app.use(express.static(path.join(__dirname, "public")));
 
+// صفحه اصلی سایت
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// صفحه ادمین
+app.get("/admin", (req, res) => {
+  res.send("Admin panel is working!");
 });
 
 app.listen(PORT, () => {
